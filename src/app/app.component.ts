bb7ed06f1609ea6678be6c923d0ca375b8ff16f3
Black-Panther30121995 <shivanikumari20050320@gmail.com>
2026-02-16 21:51:55 +0530

    angular practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,39 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-practice';
+  count:number=0;
+
+  increment(){
+    this.count++;
+  }
+  decrement(){
+    if(this.count>0){
+      this.count--;
+    }
+    else{
+      alert("Count cannot be negative");
+    }
+  }
+  reset(){
+    this.count=0;
+  }
+
+  handleCounter(val:string)
+  {
+    if(val=="increment"){
+      this.increment();
+    }
+    else if(val=="decrement"){
+      this.decrement();
+    }
+    else{
+      this.reset();
+    }
+  }
+
+  handleEvent(event:MouseEvent){
+    console.log("function called",event.type);
+    console.log("function called",(event.target as Element).classList);
+  }
+
 }
